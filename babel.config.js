79f28e99510321c321deca488c890d2085256cee
Joel Mercier <j.mercier@ideematic.com>
2025-08-13 15:13:00 +0200

    revert reanimated to v3 for skia compat

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// react-native-worklets/plugin has to be listed last.
-module.exports = (api) => {
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-		plugins: ["react-native-worklets/plugin"],
-	};
-};
