9315fcb210a139c2a79e1de8982fddc5fe9729c5
Joel Mercier <j.mercier@ideematic.com>
2025-09-15 14:46:00 +0200

    lint

--- types.ts
+++ types.ts
@@ -1,33 +1,33 @@
 import type { BarcodeScanningResult } from "expo-camera";
 
 export type ScannerResult = Omit<BarcodeScanningResult, "extra"> & {
-  extra?: Record<string, string>
-  createdAt: Date;
-  source: "scanner" | "form";
+	extra?: Record<string, string>;
+	createdAt: Date;
+	source: "scanner" | "form";
 };
 
 export type CodeType =
-  | "url"
-  | "text"
-  | "wifi"
-  | "email"
-  | "phone"
-  | "sms"
-  | "vcard"
-  | "geoPoint"
-  | "crypto"
-  | "event";
+	| "url"
+	| "text"
+	| "wifi"
+	| "email"
+	| "phone"
+	| "sms"
+	| "vcard"
+	| "geoPoint"
+	| "crypto"
+	| "event";
 export type EncryptionType = "WPA" | "WEP" | undefined;
 export type CryptoType = "bitcoin" | "ethereum" | "litecoin";
 
 export type BarcodeApiOptions = {
-  fg?: string;
-  bg?: string;
-  module?: number;
-  qz?: number;
-  pattern?: string;
-  dpi?: number;
-  height?: number;
-  text?: "bottom" | "top";
-  font?: number;
+	fg?: string;
+	bg?: string;
+	module?: number;
+	qz?: number;
+	pattern?: string;
+	dpi?: number;
+	height?: number;
+	text?: "bottom" | "top";
+	font?: number;
 };
